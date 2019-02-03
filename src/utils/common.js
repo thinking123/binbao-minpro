a7f6a5c328fdc3f5799a4c0198dc95db4675ffee
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-02-04 00:09:30 +0800

    step share page

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,12 @@
-export function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+export function numberWithCommas(x , step = 3) {
+
+  const regStr = `\\B(?=(\\d{${step}})+(?!\\d))`
+
+  console.log('reg' , regStr)
+
+  const reg = new RegExp(regStr , 'g')
+  // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(reg, ",");
 }
+
+
