def9c4e7d1e5b4aecf422a48bd2e4ff2bbb2ae64
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-02-07 11:06:03 +0800

    page

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,7 +2,7 @@ export function numberWithCommas(x , step = 3) {
 
   const regStr = `\\B(?=(\\d{${step}})+(?!\\d))`
 
-  console.log('reg' , regStr)
+  // console.log('reg' , regStr)
 
   const reg = new RegExp(regStr , 'g')
   // return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
