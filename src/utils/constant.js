5e9515b67ceeefb1bcea548a951938166829d31d
7ab_100009078201_ThinKing <7ab_100009078201_ThinKing@git.cloud.tencent.com>
2019-02-12 11:07:39 +0800

    dialog images answer dialog cdn url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -15,3 +15,90 @@ export const checkInKnowledge=[
     text:'全麦谷粒主要分为三层，包括最外层的糠，及内层的胚芽和内胚芽。'
   }
 ]
+
+
+
+
+export const answers = [
+  {
+    answerTitle:'什么是“全麦”？',
+    questions:[
+      '用没有去掉麸皮的麦类所制的食材',
+      '一种大米的名称'
+    ],
+    rightIndex:0
+  },
+  {
+    answerTitle:'以下哪种食物是全麦食品？',
+    questions:[
+      '燕麦片',
+      '牛奶'
+    ],
+    rightIndex:0
+  },
+  {
+    answerTitle:'以下哪种有助于减肥？',
+    questions:[
+      '管住嘴迈开腿',
+      '转发锦鲤'
+    ],
+    rightIndex:0
+  },
+  {
+    answerTitle:'以下哪种是全麦面包的优点？',
+    questions:[
+      '富含膳食纤维',
+      '富含脂肪'
+    ],
+    rightIndex:0
+  },
+  {
+    answerTitle:'GI值是什么意思？',
+    questions:[
+      '食物血糖生成指数',
+      '空气质量指数'
+    ],
+    rightIndex:0
+  },
+  {
+    answerTitle:'以下哪种食物是全麦食品？',
+    questions:[
+      '曼可顿全麦面包',
+      '锅包肉'
+    ],
+    rightIndex:0
+  },
+  {
+    answerTitle:'以下哪种GI值\n(食物血糖生成指数) 低？',
+    questions:[
+      '白面包',
+      '全麦面包'
+    ],
+    rightIndex:1
+  },
+  {
+    answerTitle:'GI (血糖生成指数)\n越高代表什么？',
+    questions:[
+      '糖分消化吸收的速度就越快',
+      '糖分消化吸收的速度就越慢'
+    ],
+    rightIndex:1
+  },
+  {
+    //todo 是否错误
+    answerTitle:'以下哪项不是鉴别\n全麦面包的方法？',
+    questions:[
+      '也是微褐，肉眼能看到麦麸的小粒',
+      '长得好看'
+    ],
+    rightIndex:1
+  },
+  {
+    answerTitle:'以下哪种是健康的生活方式？',
+    questions:[
+      '合理膳食',
+      '烟酒不离'
+    ],
+    rightIndex:0
+  },
+]
